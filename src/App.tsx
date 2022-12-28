f900a03b1019b6617f4b0fba4ff4064f67d64e03
marcomrdagsasc <marcomr@dagsasc.com>
2022-12-28 14:56:22 -0600

    Realm

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Realm from "realm";
 
 function App() {
+
+  useEffect(() => {
+    (async () => {
+      const app = new Realm.App({ id: "poc-kuskk" });
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      const DogSchema = {
+        name: "Dog",
+        properties: {
+          _id: 'int',
+          name: "string",
+          age: "int",
+        },
+        primaryKey: '_id'
+      };
+      const realm = await Realm.open({
+        schema: [DogSchema],
+        sync: {
+          user: user,
+          flexible: true,
+        },
+      });
+      const mongodb = app?.currentUser?.mongoClient("mongodb-atlas");
+      const submissions = await mongodb?.db("Cruzadas").collection("submissions").find();
+      console.log("submissions", submissions);
+
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
